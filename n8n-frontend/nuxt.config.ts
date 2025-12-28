3da21e848b02591dd44466482922143742f0694a
dhanur350 <dhanur297@gmail.com>
2025-12-28 16:20:42 +0530

    changes url

--- n8n-frontend/nuxt.config.ts
+++ n8n-frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  app: {
+    baseURL: "/prompt-engineering/"
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
